4b0c304447e44cf888a0d864b4be84aef102a170
Hemanth Sai Garladinne <73033596+HemanthSai7@users.noreply.github.com>
2024-05-03 22:31:42 +0530

    Update authors.py

--- components/authors.py
+++ components/authors.py
@@ -11,5 +11,8 @@ def authors():
     - Github → [@HemanthSai7](https://github.com/HemanthSai7)
     - Github → [@mayureshagashe2105](https://github.com/mayureshagashe2015)
     - Github → [@vishesht27](https://github.com/vishesht27)
+    - Github → [@Alok-Khansali](https://github.com/Alok-Khansali)
+     - Github → [@Vice777](https://github.com/Vice777)
+    
     """
     )
